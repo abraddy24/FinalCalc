491bb9cb2d4a9ee2e25f982814d282909527a998
Alex Braddy <braddya16@hanover.edu>
2015-03-16 11:45:03 -0400

    edited index and main files to test for working. does not yet

--- app/main.js
+++ app/main.js
@@ -1,1 +1,25 @@
-console.log("main.js is loaded");
+// Main file that loads things up.
+// Called from require.js
+
+require.config({
+   // baseUrl is automatically set to the directory where this main.js file is.
+   // Or we can set it in this config:
+   baseUrl: "app",
+   // This way one can call on jquery and libs directly
+   paths: {
+      "lib": "../lib",
+      "jquery": "../lib/jquery-2.1.3"
+      // or the following to load remotely:
+      // "jquery": "https://code.jquery.com/jquery-2.1.3.min"
+   },
+   waitSeconds: 15
+});
+
+// All other modules should be called through here
+require(["jquery"],
+function($, mixin, structures) {
+   // mixin
+   $(function() {
+      console.log("Page loaded!");
+   });
+});
